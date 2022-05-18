1cc7e8e3e7236737f2020bcda5e9636d95699e06
rezwanrahimr <rezwanrahim69@gmail.com>
2022-05-19 03:37:53 +0600

    added items

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import url from '../../Api';
@@ -7,8 +8,8 @@ import auth from '../../firebase.init';
 const AddTask = () => {
     const navigate = useNavigate();
     const [user] = useAuthState(auth);
-    const [value,setValue] = useState({title:'',description:''});
-    console.log(user)
+    const [value, setValue] = useState({ title: '', description: '' });
+
     const addTask = (e) => {
         e.preventDefault();
         const body = {
@@ -16,32 +17,38 @@ const AddTask = () => {
             status: "publish",
             email: user.email
         }
-        fetch(url+'/todos',{
+        fetch(url + '/todos', {
             method: "POST",
-            headers: {'Content-Type': 'application/json'},
-            body:JSON.stringify(body)
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
 
         })
-        .then(res => res.json())
-        .then(result => {
-            if(result.status===1){
-                navigate('/home')
-            }
-        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.status === 1) {
+                    navigate('/home')
+                }
+            })
+    }
+    const handleInput = (e) => {
+        setValue(prev => ({ ...prev, [e.target.name]: e.target.value }))
     }
 
-    const handleInput = (e) =>{
-        setValue(prev=>({...prev,[e.target.name]: e.target.value}))
-
-    }
+    
     return (
-        <div>
-            <h6>add task </h6>
-            <form onSubmit={addTask}>
-                <input name='title' onChange={handleInput} type="text" />
-                <textarea name="description"  onChange={handleInput} id="" cols="30" rows="10"></textarea>
-                <button type='submit'>submit</button>
-            </form>
+        <div className='d-flex justify-content-center align-items-center mt-5'>
+            <Card style={{ width: '30rem' }}>
+                <Card.Body>
+                    <Form onSubmit={addTask}>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Control name='title' type="text" placeholder="Name" onChange={handleInput} />
+                            
+                            <Form.Control name="description" onChange={handleInput} className='my-3' placeholder='Description' as="textarea" rows={3} />
+                            <Button type='submit' variant="warning">Submit</Button>{' '}
+                        </Form.Group>
+                    </Form>
+                </Card.Body>
+            </Card>
 
         </div>
     );
